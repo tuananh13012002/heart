17013c25fc3b51a026b14c9cc3bbd984005c258c
tuananh13012002 <tuananh13160108@gamil.com>
2022-11-08 23:22:40 +0700

    convert image

---
+++ js2.js
@@ -0,0 +1,85 @@
+myImage.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 706;
+  ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+  const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  let particlesArray = [];
+  const numberOfParticles = 5000;
+  let mappedImage = [];
+  for (let y = 0; y < canvas.height; y++) {
+    let row = [];
+    for (let x = 0; x < canvas.width; x++) {
+      const red = pixels.data[y * 4 * pixels.width + x * 4];
+      const green = pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
+      const blue = pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      const cell = [(cellBrightness = brightness)];
+      row.push(cell);
+    }
+    mappedImage.push(row);
+  }
+  function calculateRelativeBrightness(red, green, blue) {
+    return (
+      Math.sqrt(
+        red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
+      ) / 100
+    );
+  }
+  class Particle {
+    constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = 0;
+      this.speed = 0;
+      this.velocity = Math.random() * 3.5;
+      this.size = Math.random() * 1.5 + 1;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+    }
+    update() {
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      this.speed == mappedImage[this.position1][this.position2][0];
+
+      let movement = 2.5 - this.speed + this.velocity;
+      this.y += movement;
+      if (this.y >= canvas.height) {
+        this.y = 0;
+        this.x = Math.random() * canvas.width;
+      }
+    }
+    draw() {
+      ctx.beginPath();
+      /*if ((mappedImage[this.position1])&&(mappedImage[this.position1][this.position2])){
+                ctx.fillStyle = mappedImage[this.position1][this.position2][1];
+                ctx.strokeStyle = mappedImage[this.position1][this.position2][1];
+            }*/
+      // ctx.fillStyle = gradient1;
+      ctx.fillStyle = "white";
+      //ctx.strokeRect(this.x, this.y, this.size * 3, this.size * 3);
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+  function init() {
+    for (let i = 0; i < numberOfParticles; i++) {
+      particlesArray.push(new Particle());
+    }
+  }
+  init();
+  function animate() {
+    ctx.globalAlpha = 0.05;
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.2;
+    for (let i = 0; i < particlesArray.length; i++) {
+      particlesFArray[i].update();
+      ctx.globalAlpha = particlesArray[i].speed * 0.5;
+      //ctx.globalAlpha = 1;
+      particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
